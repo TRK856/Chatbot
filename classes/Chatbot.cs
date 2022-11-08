ba2cbc86698e37f5534908044f6901ec83925b7b
TRK856 <53193704+TRK856@users.noreply.github.com>
2022-11-08 09:58:11 -0700

    Json

--- classes/Chatbot.cs
+++ classes/Chatbot.cs
@@ -3,6 +3,7 @@ public class Chatbot
     public string catagory { get; set; }
     public List<string> userInputs { get; set; }
     public List<string> botResponses { get; set; }
+
     public Chatbot(string catagory, List<string> userInputs, List<string> botResponses)
     {
         this.catagory = catagory;
